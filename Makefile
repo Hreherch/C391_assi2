4616e60dbd6bb536a1c19309a68ea8b9a0d84450
hreherch <hreherch@ualberta.ca>
2016-10-18 22:01:00 -0600

    Added some cool stuff guys 1018

---
+++ Makefile
@@ -0,0 +1,2 @@
+q4: q4.c
+	gcc -g q4.c -o q4 sqlite3.c -lpthread -ldl
---
+++ q4.c
@@ -0,0 +1,16 @@
+#include "sqlite3.h"
+#include <stdio.h>
+
+#define DB_NAME "A2.db"
+
+int main( int argc, char **argv ) {
+    sqlite3 *db;
+    char *sql_str;
+    
+    if( 0 != sqlite3_open( DB_NAME, &db ) ) {
+        fprintf( stderr, "Failed to open DB(is DB in folder?)" );
+        sqlite3_close( db );
+        return( 1 );
+    }
+    
+} //main 
